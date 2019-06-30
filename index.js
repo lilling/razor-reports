bf8e53109ca25b395e6c9a7d760f59dfd244003b
Yonatan Liling <Yonatan.Liling@cellebrite.com>
2019-06-30 11:41:14 +0300

    2nd commit

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const Excel = require('exceljs/modern.nodejs');
 const fs = require('fs');
 
 if (!process.argv[2]) {
-    console.error('no file to load')
+    console.error('no .xlsx file to load');
     return;
 }
 
-var dateParser = function (day, time) {
+const dateParser = function (day, time) {
     let hour = time.getUTCHours().toString();
     if (hour.length === 1) {
         hour = '0' + hour;
@@ -17,53 +17,58 @@ var dateParser = function (day, time) {
         minutes = '0' + minutes;
     }
     return day + 'T' + hour + ':' + minutes;
-}
+};
 
-fs.readFile('user-data', (err, data) => {
-    if(err) {
-        console.error('make sure you have user-data file')
-        return;
-    }
-    const cred = data.toString().split('\r\n').reduce((result, current) => {
-        const prop = current.split(':');
-        if (prop[1]) {
-            result[prop[0]] = prop[1];
+const bootstrap = function () {
+    fs.readFile('user-data', (err, data) => {
+        if(err) {
+            console.error('make sure you have user-data file')
+            return;
         }
-        return result;
-    }, {});
+        const cred = data.toString().split('\r\n').reduce((result, current) => {
+            const prop = current.split(':');
+            if (prop[1]) {
+                result[prop[0]] = prop[1];
+            }
+            return result;
+        }, {});
 
-    axios.post('https://hour.razorgrip.com/login', cred)
-        .then(res => {
-            const { token, userId } = res.data;
-            axios.defaults.headers.common = {'Authorization': `Bearer ${token}`};
+        axios.post('https://hour.razorgrip.com/login', cred)
+            .then(res => {
+                const { token, userId } = res.data;
+                axios.defaults.headers.common = {'Authorization': `Bearer ${token}`};
 
-            const workbook = new Excel.Workbook();
-            workbook.xlsx.readFile(process.argv[2])
-                .then(() => {
-                    const worksheet = workbook.getWorksheet(1);
-                    worksheet.eachRow((row, rowNumber) => {
-                        if (rowNumber < 4)
-                            return;
-                        const day = new Date().getFullYear() + '-' + row.values[1].split(' ')[1].split('/').reverse().join('-');
-                        const from_date = dateParser(day, row.values[2] || row.values[5]);
-                        const to_date = dateParser(day, row.values[3] || row.values[6]);
-                        const data = {
-                            user_id: userId,
+                const workbook = new Excel.Workbook();
+                workbook.xlsx.readFile(process.argv[2])
+                    .then(() => {
+                        const worksheet = workbook.getWorksheet(1);
+                        worksheet.eachRow((row, rowNumber) => {
+                            if (rowNumber < 4)
+                                return;
+                            const day = new Date().getFullYear() + '-' + row.values[1].split(' ')[1].split('/').reverse().join('-');
+                            const from_date = dateParser(day, row.values[2] || row.values[5]);
+                            const to_date = dateParser(day, row.values[3] || row.values[6]);
+                            const data = {
+                                user_id: userId,
                                 project_id: 40,
                                 from_date,
                                 to_date
-                        };
-                        axios.post('https://hour.razorgrip.com/hourreports/save',{ data })
-                            .then(response => {
-                                console.log(response);
-                            })
-                            .catch(error => {
-                                console.log(error);
-                            });
+                            };
+                            axios.post('https://hour.razorgrip.com/hourreports/save',{ data })
+                                .then(response => {
+                                    console.log(response);
+                                })
+                                .catch(error => {
+                                    console.log(error);
+                                });
+                        });
                     });
-                });
-        })
-        .catch(error => {
-            console.error(error)
-        })
-});
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    });
+};
+
+bootstrap();
+

0ee6bca0ce4900bfa81bb53b95c0d50dd248417d
Yonatan Liling <Yonatan.Liling@cellebrite.com>
2019-08-29 10:47:26 +0300

    add vacations

--- index.js
+++ index.js
@@ -7,24 +7,31 @@ if (!process.argv[2]) {
     return;
 }
 
-const dateParser = function (day, time) {
-    let hour = time.getUTCHours().toString();
-    if (hour.length === 1) {
-        hour = '0' + hour;
+const dateParser = function (day, time = null) {
+    let minutes = '00';
+    let hour = '00';
+    if (time) {
+        hour = time.getUTCHours().toString();
+        if (hour.length === 1) {
+            hour = '0' + hour;
+        }
+        minutes = time.getUTCMinutes().toString();
+        if (minutes.length === 1) {
+            minutes = '0' + minutes;
+        }
     }
-    minutes = time.getUTCMinutes().toString();
-    if (minutes.length === 1) {
-        minutes = '0' + minutes;
-    }
+
     return day + 'T' + hour + ':' + minutes;
 };
 
 const bootstrap = function () {
+    const url = 'https://hour.razorgrip.com/';
     fs.readFile('user-data', (err, data) => {
         if(err) {
             console.error('make sure you have user-data file')
             return;
         }
+		
         const cred = data.toString().split('\r\n').reduce((result, current) => {
             const prop = current.split(':');
             if (prop[1]) {
@@ -33,9 +40,11 @@ const bootstrap = function () {
             return result;
         }, {});
 
-        axios.post('https://hour.razorgrip.com/login', cred)
+        axios.post(url + 'login', cred)
             .then(res => {
                 const { token, userId } = res.data;
+                const vacation = url + 'hourreports/saveVacation';
+                const regular = url + 'hourreports/save';
                 axios.defaults.headers.common = {'Authorization': `Bearer ${token}`};
 
                 const workbook = new Excel.Workbook();
@@ -43,18 +52,18 @@ const bootstrap = function () {
                     .then(() => {
                         const worksheet = workbook.getWorksheet(1);
                         worksheet.eachRow((row, rowNumber) => {
-                            if (rowNumber < 4)
-                                return;
+                            if (rowNumber < 4) return;
                             const day = new Date().getFullYear() + '-' + row.values[1].split(' ')[1].split('/').reverse().join('-');
-                            const from_date = dateParser(day, row.values[2] || row.values[5]);
-                            const to_date = dateParser(day, row.values[3] || row.values[6]);
-                            const data = {
-                                user_id: userId,
-                                project_id: 40,
-                                from_date,
-                                to_date
-                            };
-                            axios.post('https://hour.razorgrip.com/hourreports/save',{ data })
+                            let data = { user_id: userId, project_id: 40 };
+
+                            if (row.values[9] === 'חופשה') {
+                                data.date = dateParser(day);
+                            } else {
+                                data.from_date = dateParser(day, row.values[2] || row.values[5]);
+                                data.to_date = dateParser(day, row.values[3] || row.values[6]);
+                            }
+
+                            axios.post(data.date ? vacation : regular,{ data })
                                 .then(response => {
                                     console.log(response);
                                 })
